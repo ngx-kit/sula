96d6f247dd4ede9777dba75477ea1859c3ffb564
Kate Novyk <icydew@live.com>
2018-05-15 20:10:38 +0300

    feat(ui): alerts styles

--- projects/ui/src/lib/ui-alert/demo/ui-alert-demo.component.ts
+++ projects/ui/src/lib/ui-alert/demo/ui-alert-demo.component.ts
@@ -5,7 +5,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ui-alert-demo.component.scss'],
 })
 export class UiAlertDemoComponent implements OnInit {
-  isOpen = true;
+  isOpen1 = true;
+  isOpen2 = true;
+  isOpen3 = true;
+  isOpen4 = true;
+  isOpen5 = true;
+  isOpen6 = true;
 
   constructor() {
   }
--- projects/ui/src/lib/ui-alert/demo/ui-alert-demo.module.ts
+++ projects/ui/src/lib/ui-alert/demo/ui-alert-demo.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UiAlertModule } from '../ui-alert.module';
 import { UiAlertDemoComponent } from './ui-alert-demo.component';
+import { UiButtonModule } from 'ui';
 
 @NgModule({
   imports: [
     CommonModule,
     UiAlertModule,
+    UiButtonModule,
   ],
   declarations: [UiAlertDemoComponent],
 })
