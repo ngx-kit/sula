96db89b98c223e27b5a3e13d7c543ca8e4ae9197
Kate Novyk <icydew@live.com>
2018-06-05 11:59:50 +0300

    feat(ui): autocomplete styles

--- projects/ui/src/lib/ui-autocomplete/demo/ui-autocomplete-demo.module.ts
+++ projects/ui/src/lib/ui-autocomplete/demo/ui-autocomplete-demo.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UiAutocompleteModule } from '../ui-autocomplete.module';
 import { UiAutocompleteDemoComponent } from './ui-autocomplete-demo.component';
+import { UiTextModule } from '../../ui-text/ui-text.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     UiAutocompleteModule,
+    // Combined demo
+    UiTextModule,
   ],
   declarations: [
     UiAutocompleteDemoComponent,
