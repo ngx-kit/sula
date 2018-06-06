e9eacc1046197c3dab1c3e4388dd20215f0004d1
Kate Novyk <icydew@live.com>
2018-06-06 14:48:26 +0300

    feat(ui): tooltip styles

--- projects/ui/src/lib/ui-tooltip/demo/ui-tooltip-demo.module.ts
+++ projects/ui/src/lib/ui-tooltip/demo/ui-tooltip-demo.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UiTooltipModule } from '../ui-tooltip.module';
 import { UiTooltipDemoComponent } from './ui-tooltip-demo.component';
+import { UiButtonModule } from 'ui';
 
 @NgModule({
   imports: [
     CommonModule,
     UiTooltipModule,
+    UiButtonModule,
   ],
   declarations: [UiTooltipDemoComponent],
   exports: [],
