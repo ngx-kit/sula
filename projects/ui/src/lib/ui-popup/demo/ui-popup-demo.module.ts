cf05ac47e6ec8e7e99e18ad10fee1c12f429ef6f
Kate Novyk <icydew@live.com>
2018-06-11 15:31:28 +0300

    feat(ui): popup styles

--- projects/ui/src/lib/ui-popup/demo/ui-popup-demo.module.ts
+++ projects/ui/src/lib/ui-popup/demo/ui-popup-demo.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiPopupModule } from '../ui-popup.module';
 import { UiPopupDemoComponent } from './ui-popup-demo.component';
+import { UiButtonModule } from 'ui';
 
 @NgModule({
   imports: [
     CommonModule,
     UiPopupModule,
+    UiButtonModule,
   ],
   declarations: [
     UiPopupDemoComponent,
