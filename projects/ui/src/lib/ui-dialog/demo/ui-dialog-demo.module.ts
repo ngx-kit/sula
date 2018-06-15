ee8ac0d63d4934453cd0fa65278b2b414ff2e947
Kate Novyk <icydew@live.com>
2018-06-15 16:05:23 +0300

    feat(ui): dialog styles

--- projects/ui/src/lib/ui-dialog/demo/ui-dialog-demo.module.ts
+++ projects/ui/src/lib/ui-dialog/demo/ui-dialog-demo.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UiDialogModule } from '../ui-dialog.module';
 import { UiDialogDemoComponent } from './ui-dialog-demo.component';
+import { UiButtonModule } from 'ui';
+import { UiTextModule } from '../../ui-text/ui-text.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     UiDialogModule,
+    UiButtonModule,
+    UiTextModule,
   ],
   declarations: [
     UiDialogDemoComponent,
--- projects/ui/src/lib/ui-dialog/ui-dialog.module.ts
+++ projects/ui/src/lib/ui-dialog/ui-dialog.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { KitClassModule } from '@ngx-kit/core';
 import { UiDialogComponent } from './ui-dialog/ui-dialog.component';
+import { UiButtonModule } from 'ui';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     KitClassModule,
+    UiButtonModule,
   ],
   declarations: [
     UiDialogComponent,
