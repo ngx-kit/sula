75f8a38b4787dc6773c5872e650c94d65474c30a
Sasha Novyk <inavix@gmail.com>
2018-06-15 13:42:36 +0300

    feat(ui): add UiDialog module

--- projects/ui-docs/src/app/components/components-routing.module.ts
+++ projects/ui-docs/src/app/components/components-routing.module.ts
@@ -8,6 +8,7 @@ import { UiBreadcrumbsDemoComponent } from '../../../../ui/src/lib/ui-breadcrumb
 import { UiButtonDemoComponent } from '../../../../ui/src/lib/ui-button/demo/ui-button-demo.component';
 import { UiCheckboxDemoComponent } from '../../../../ui/src/lib/ui-checkbox/demo/ui-checkbox-demo.component';
 import { UiCustomSelectDemoComponent } from '../../../../ui/src/lib/ui-custom-select/demo/ui-custom-select-demo.component';
+import { UiDialogDemoComponent } from '../../../../ui/src/lib/ui-dialog/demo/ui-dialog-demo.component';
 import { UiDrawerDemoComponent } from '../../../../ui/src/lib/ui-drawer/demo/ui-drawer-demo.component';
 import { UiDropdownDemoComponent } from '../../../../ui/src/lib/ui-dropdown/demo/ui-dropdown-demo.component';
 import { UiFormDemoComponent } from '../../../../ui/src/lib/ui-form/demo/ui-form-demo.component';
@@ -59,6 +60,10 @@ const routes: Routes = [
       {
         path: 'ui-custom-select',
         component: UiCustomSelectDemoComponent,
+      },
+      {
+        path: 'ui-dialog',
+        component: UiDialogDemoComponent,
       },
       {
         path: 'ui-drawer',
--- projects/ui-docs/src/app/components/components.module.ts
+++ projects/ui-docs/src/app/components/components.module.ts
@@ -8,6 +8,7 @@ import { UiBreadcrumbsDemoModule } from '../../../../ui/src/lib/ui-breadcrumbs/d
 import { UiButtonDemoModule } from '../../../../ui/src/lib/ui-button/demo/ui-button-demo.module';
 import { UiCheckboxDemoModule } from '../../../../ui/src/lib/ui-checkbox/demo/ui-checkbox-demo.module';
 import { UiCustomSelectDemoModule } from '../../../../ui/src/lib/ui-custom-select/demo/ui-custom-select-demo.module';
+import { UiDialogDemoModule } from '../../../../ui/src/lib/ui-dialog/demo/ui-dialog-demo.module';
 import { UiDrawerDemoModule } from '../../../../ui/src/lib/ui-drawer/demo/ui-drawer-demo.module';
 import { UiDropdownDemoModule } from '../../../../ui/src/lib/ui-dropdown/demo/ui-dropdown-demo.module';
 import { UiFormDemoModule } from '../../../../ui/src/lib/ui-form/demo/ui-form-demo.module';
@@ -38,6 +39,7 @@ import { ComponentsRoutingModule } from './components-routing.module';
     UiButtonDemoModule,
     UiCheckboxDemoModule,
     UiCustomSelectDemoModule,
+    UiDialogDemoModule,
     UiDrawerDemoModule,
     UiDropdownDemoModule,
     UiFormDemoModule,
