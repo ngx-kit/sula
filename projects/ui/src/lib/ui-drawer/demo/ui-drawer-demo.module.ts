3f2c59c62e3e065f2d4d826b0192574d50adad34
Kate Novyk <icydew@live.com>
2018-06-16 10:39:56 +0300

    feat(ui): drawer styles

--- projects/ui/src/lib/ui-drawer/demo/ui-drawer-demo.module.ts
+++ projects/ui/src/lib/ui-drawer/demo/ui-drawer-demo.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiDrawerModule } from '../ui-drawer.module';
 import { UiDrawerDemoComponent } from './ui-drawer-demo.component';
+import { UiButtonModule } from 'ui';
 
 @NgModule({
   imports: [
     CommonModule,
     UiDrawerModule,
+    UiButtonModule,
   ],
   declarations: [
     UiDrawerDemoComponent,
