718cd9332945b24d386381af445bb95ed2d8321c
Kate Novyk <icydew@live.com>
2018-06-25 18:46:33 +0300

    feat(ui): date-picker styles, loading-bar styles, rating styles

--- projects/ui/src/lib/ui-loading-bar/demo/ui-loading-bar-demo.module.ts
+++ projects/ui/src/lib/ui-loading-bar/demo/ui-loading-bar-demo.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UiLoadingBarModule } from '../ui-loading-bar.module';
 import { UiLoadingBarDemoComponent } from './ui-loading-bar-demo.component';
+import { UiButtonModule } from 'ui';
 
 @NgModule({
   imports: [
     CommonModule,
     UiLoadingBarModule,
+    UiButtonModule,
   ],
   declarations: [
     UiLoadingBarDemoComponent,
