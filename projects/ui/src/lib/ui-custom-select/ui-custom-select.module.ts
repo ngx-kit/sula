ac03686f10019b06081c4f57d8a3ec2bf0eb3c31
Sasha Novyk <inavix@gmail.com>
2018-07-11 15:40:31 +0300

    fix(ui): update custom-select

--- projects/ui/src/lib/ui-custom-select/ui-custom-select.module.ts
+++ projects/ui/src/lib/ui-custom-select/ui-custom-select.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { KitOverlayModule, KitPositionModule } from '@ngx-kit/core';
+import { KitIconsModule, KitOutsideClickModule, KitOverlayModule, KitPositionModule } from '@ngx-kit/core';
 import { UiCustomSelectComponent } from './ui-custom-select/ui-custom-select.component';
 
 @NgModule({
@@ -8,6 +8,8 @@ import { UiCustomSelectComponent } from './ui-custom-select/ui-custom-select.com
     CommonModule,
     KitOverlayModule,
     KitPositionModule,
+    KitOutsideClickModule,
+    KitIconsModule,
   ],
   declarations: [
     UiCustomSelectComponent,
