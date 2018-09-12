ff1169141de0994e7560fb2cd1b72064c9b98f0b
Sasha Novyk <inavix@gmail.com>
2018-09-12 10:07:53 +0300

    docs: Fix missing icons in demos

--- projects/ui/src/lib/ui-rating/demo/ui-rating-demo.component.ts
+++ projects/ui/src/lib/ui-rating/demo/ui-rating-demo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KitIconsRegistryService } from '@ngx-kit/core';
 
 /**
  * @demo
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './ui-rating-demo.component.html',
 })
 export class UiRatingDemoComponent {
+  constructor(private icons: KitIconsRegistryService) {
+    this.icons.add({
+      name: 'star',
+      xml: '<svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8"><path d="M4 0l-1 3h-3l2.5 2-1 3 2.5-2 2.5 2-1-3 2.5-2h-3l-1-3z"/></svg>',
+    });
+  }
 }
